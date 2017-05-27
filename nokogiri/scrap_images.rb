0225e227a72fbf4e82c7ea48013dd24e584f80ac
Mycroft1891 <mycroft1891@gmail.com>
2017-05-27 00:51:23 -0500

    Update Nokogiri script to use Mechanize gem

--- nokogiri/scrap_images.rb
+++ nokogiri/scrap_images.rb
@@ -1,13 +1,16 @@
 require 'nokogiri'
 require 'httparty'
 require 'open-uri'
+require 'mechanize'
 
 main_url = ARGV[0]
+folder = ARGV[1]
+agent = Mechanize.new
 
 Nokogiri::HTML(open(main_url)).xpath('//a/@href').each do |url|
-    puts url
-    Nokogiri::HTML(open(url)).xpath("//img/@src").each do |src|
-        uri = URI.join( url, src ).to_s # make absolute uri
-        File.open(File.basename(uri),'wb'){ |f| f.write(open(uri).read) }
-    end
+  puts url
+  Nokogiri::HTML(open(url)).xpath("//img/@src").each do |src|
+      uri = URI.join( url, src ).to_s
+      agent.get(src).save "#{folder}/#{File.basename(uri)}"
+  end
 end
