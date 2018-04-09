0913c302acf1eecc9693009f6cd90cb26e344452
Mycroft1891 <mycroft1891@gmail.com>
2018-04-09 14:01:29 +0200

    script to rename sequntial files

---
+++ nokogiri/rename_files.rb
@@ -0,0 +1,11 @@
+base_name = ARGV[0]
+index = 0
+my_dir = Dir.open(Dir.pwd).sort_by{|f| File.mtime(f) }
+my_dir.sort.each do |filename|
+		next if File.directory?(filename) or filename == ".DS_Store"
+		index += 1
+		ep = index < 10 ? "0#{index}" : "#{index}"
+		extention = File.extname(filename)
+		File.rename(filename, base_name + ep + extention)
+		#puts "File: #{filename} \tRenamed: #{base_name + ep + extention}"
+end
