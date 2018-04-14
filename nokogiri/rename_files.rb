bed4bb5770d3f4149b8590d4c2a399f83deef31d
Mycroft1891 <mycroft1891@gmail.com>
2018-04-14 20:19:36 +0200

    remove sort funtion and add new CLI argument

--- nokogiri/rename_files.rb
+++ nokogiri/rename_files.rb
@@ -1,10 +1,9 @@
 base_name = ARGV[0]
-index = 0
-my_dir = Dir.open(Dir.pwd).sort_by{|f| File.mtime(f) }
-my_dir.sort.each do |filename|
+index = ARGV[1].to_i || 0
+Dir.open(Dir.pwd).sort.each do |filename|
 		next if File.directory?(filename) or filename == ".DS_Store"
 		index += 1
-		ep = index < 10 ? "0#{index}" : "#{index}"
+		ep = index < 10 ? "0#{index}" : index.to_s
 		extention = File.extname(filename)
 		File.rename(filename, base_name + ep + extention)
 		puts "File: #{filename} \tRenamed: #{base_name + ep + extention}"
