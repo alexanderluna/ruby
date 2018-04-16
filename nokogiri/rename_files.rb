556b9c0a2b0d33c91eec850fc950e56e7a547f25
Mycroft1891 <mycroft1891@gmail.com>
2018-04-16 21:18:51 +0200

    clean up and make CLI friendly

--- nokogiri/rename_files.rb
+++ nokogiri/rename_files.rb
@@ -1,22 +1,22 @@
+#!/usr/bin/ruby
+abort("Missing base name argument") if ARGV[0].nil?
+
 def list_and(option={})
-	base_name = ARGV[0]
-	index = ARGV[1].to_i || 0
+	base_name, index = ARGV[0], ARGV[1].to_i || 0
 	Dir.open(Dir.pwd).sort.each do |filename|
 		next if File.directory?(filename) or filename == ".DS_Store"
 		index += 1
-		ep = index < 10 ? "0#{index}" : index.to_s
-		extention = File.extname(filename)
-		File.rename(filename, base_name + ep + extention) if option[:rename]
-		puts "File: #{filename} \tRenamed: #{base_name + ep + extention}"
+		new_name = base_name + index.to_s.rjust(2, '0') + File.extname(filename)
+		File.rename(filename, new_name) if option[:rename]
+		puts "File: #{filename} \tRenamed: #{new_name}"
 	end
 end
 
-puts "***** CHECK THE OUPUT *****\n"
 list_and(sort: true)
 print "\nDo you want to rename the files ? (y/n): "
 if STDIN.gets.chomp.strip.downcase == "y"
 	list_and(rename: true)
 	abort("Done renaming files")
-else
-	abort("Nothing to do here...")
 end
+
+abort("Nothing to do here...")
