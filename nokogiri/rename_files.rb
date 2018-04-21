73ec77c3b2a9d9f193656e39df7692f9b04f8e1f
Mycroft1891 <mycroft1891@gmail.com>
2018-04-21 20:58:55 +0200

    fix 1+ error

--- nokogiri/rename_files.rb
+++ nokogiri/rename_files.rb
@@ -5,10 +5,10 @@ def list_and(option={})
 	base_name, index = ARGV[0], ARGV[1].to_i || 0
 	Dir.open(Dir.pwd).sort.each do |filename|
 		next if File.directory?(filename) or filename == ".DS_Store"
-		index += 1
 		new_name = base_name + index.to_s.rjust(2, '0') + File.extname(filename)
 		File.rename(filename, new_name) if option[:rename]
 		puts "File: #{filename} \tRenamed: #{new_name}"
+		index += 1
 	end
 end
 
