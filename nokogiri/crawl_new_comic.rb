9a2d08d32ade4ef970b1ad1b2acd1e1c7b058799
Mycroft1891 <mycroft1891@gmail.com>
2018-04-27 03:59:01 +0200

    increase readability and fix bugs

--- nokogiri/crawl_new_comic.rb
+++ nokogiri/crawl_new_comic.rb
@@ -1,31 +1,45 @@
 require 'mechanize'
 require 'rmagick'
+require 'fileutils'
 
 main_url = ARGV[0]
-name = ARGV[1]
+name = ARGV[1] + ".txt"
+fetch_chapters = "phantomjs ~/.my_scripts/crawl_site.js"
+fetch_images = "phantomjs ~/.my_scripts/loadsite.js"
 
 abort("Missing Argument URL or NAME") unless main_url and name
 
-puts "=> Loading Chapters"
-system("phantomjs ~/.my_scripts/crawl_site.js #{main_url} > #{name}.txt")
-
-chapter_list = File.open("#{name}.txt").to_a.reverse
-chapter_list.each_with_index do |link, index|
-  puts "=> Downloading Images\n#{link}"
-  tmp_file = index.to_s + ".txt"
-  system("phantomjs ~/.my_scripts/loadsite.js #{link} #{tmp_file}")
-
-  chapter = File.read(tmp_file).split(',')
-  chapter.each_with_index do |image, i|
-    agent = Mechanize.new
-    image_name = i.to_s.rjust(3, '0') + File.extname(image)
-    agent.get(image).save("#{index}/#{image_name}")
-    puts "=> Creating PDF"
-    files = Dir.open(index.to_s).sort.map { |file| "#{index}/#{file}"}
-    pdf = Magick::ImageList.new(*files)
-    pdf.write(name + index.to_s.rjust(3, "0") + ".pdf")
-  end
-  puts "=> Removing tmp files and folder"
-  system("rm -r #{index}")
-  system("rm #{tmp_file}")
+def save_img(folder, name, img)
+  sleep 5
+  print "."
+  agent = Mechanize.new
+  img_name = name.to_s.rjust(3, '0') + File.extname(img)
+  agent.get(img).save("#{folder}/#{img_name}")
 end
+
+def create_pdf_from_folder(folder)
+  puts "\n=> Creating PDF"
+  files = Dir.open(folder).sort.map { |f| "#{folder}/#{f}" if f.match('[0-9]') }
+  pdf = Magick::ImageList.new(*files.compact!)
+  pdf.write(folder.rjust(3, "0") + ".pdf")
+end
+
+puts "=> Loading Chapters"
+system("#{fetch_chapters} #{main_url} > #{name}")
+
+chapter_list = File.open(name).to_a.reverse
+chapter_list.each_with_index do |link, index|
+  tmp_file = index.to_s + ".txt"
+  puts "=> Downloading Chapter: #{index}\n#{link}"
+  system("#{fetch_images} #{link.chomp} #{tmp_file}")
+
+  img_list = File.read(tmp_file).split(',')
+  img_list.each_with_index { |img, i| save_img(index, i, img) }
+
+  create_pdf_from_folder(index.to_s)
+  FileUtils.remove_dir(index.to_s)
+  File.delete(tmp_file)
+end
+
+File.delete(name)
+abort("=> Finished Download")
