6a4139bc16247bc003f953504dd3ddfe622ad18d
Mycroft1891 <mycroft1891@gmail.com>
2018-04-30 23:29:00 +0200

    reformat to use helper module

--- nokogiri/crawl_comic.rb
+++ nokogiri/crawl_comic.rb
@@ -2,52 +2,36 @@
 
 require 'nokogiri'
 require 'open-uri'
-require 'mechanize'
-require 'rmagick'
+require_relative 'helper'
 
 main_url = ARGV[0]
 start = ARGV[1].to_i || false
 name = main_url.split('/')[4]
-chapter_list = []
 
 abort("Missing arguments: I need a link") unless main_url
 
-Nokogiri::HTML(open(main_url)).css('.color_0077').each { |elem| chapter_list.push(elem['href'].to_s) }
-
-chapter_list.map! { |i| "http:" + i if i.match?(name) }.compact!
-chapter_list.reverse!.pop
-
-def fetch_chapter(chapter_index, chapter)
-  images, agent = [], Mechanize.new
-  Nokogiri::HTML(open(chapter)).css('select.wid60 option').each { |page| images.push(page['value']) }
-  images.uniq!.map! { |i| "http:#{i}" }
-  images.each_with_index do |img, index|
-    Nokogiri::HTML(open(img)).css("img#image").each do |src|
-      print "."
-      begin
-        tries ||= 0
-        agent.get(src['src']).save("#{chapter_index}/#{index.to_s.rjust(2, '0')}.jpg")
-      rescue
-        retry if (tries += 1) < 5
-      end
-    end
-  end
-end
-
-chapter_list.each_with_index do |chapter, index|
+chapters = Nokogiri::HTML(open(main_url)).css('.color_0077')
+chapters.map! { |a| "http:" + a['href'].to_s }
+chapters.select! { |link| link.match(name) }
+chapter.compact!.reverse!.pop
+chapters.map! { |i| "http:" + i if i.match?(name) }.compact!.reverse!.pop
+chapters.each_with_index do |chapter, index|
   next if start and index < start
   begin
     tries ||= 0
     puts "\n=> Fetching Chapter #{index}\n=> #{chapter}"
-    fetch_chapter(index, chapter)
+    imgs = Nokogiri::HTML(open(chapter)).css('select.wid60 option')
+    imgs.uniq!.map! { |page| "http:" + page['value'].to_s }
+    imgs.each_with_index do |img, i|
+      Nokogiri::HTML(open(img)).css("img#image").each do |src|
+        Helper.download_image(name: i, link: src['src'], folder: chapter_index)
+      end
+    end
   rescue
     system("rm -r #{index}")
     retry if (tries += 1) < 10
   end
-
-  files = Dir.open(index.to_s).sort.map { |n| "#{index}/#{n}" if n.match?('[0-9]') }.compact
-  pdf = Magick::ImageList.new(*files)
-  pdf.write(name + index.to_s.rjust(3, "0") + ".pdf")
+  Helper.create_pdf_from_dir(index.to_s)
   system("rm -r #{index}")
 end
 
