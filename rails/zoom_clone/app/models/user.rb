b0b284005f1ac402f3630f79f97770044f163101
Alexander Luna <alexanderluna1891@gmail.com>
2020-05-29 17:47:59 +0200

    🚧 Add: graphql setup and models

--- rails/zoom_clone/app/models/user.rb
+++ rails/zoom_clone/app/models/user.rb
@@ -1,2 +1,19 @@
 class User < ApplicationRecord
+
+  has_many :microposts, dependent: :destroy
+  has_many :comments, dependent: :destroy
+
+  attr_accessor :remember_token, :activation_token, :reset_token
+  attr_accessor :skip_validation
+
+  before_save :downcase_email, :unless => :skip_validation
+
+  validates :name, presence: true
+  validates :email, presence: true
+
+  # has_secure_password
+
+  def downcase_email
+    self.email = email.downcase
+  end
 end
