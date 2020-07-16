da2b1fe7c864adb9fd715e7d2ff06f9a5b279909
Alexander Luna <alexanderluna1891@gmail.com>
2020-07-16 06:06:09 +0200

    Add: Acccount Activation

--- rails/rails_app/app/controllers/sessions_controller.rb
+++ rails/rails_app/app/controllers/sessions_controller.rb
@@ -1,13 +1,19 @@
 class SessionsController < ApplicationController
+
   def new
   end
 
   def create
     @user = User.find_by(email: params[:session][:email].downcase)
     if @user && @user.authenticate(params[:session][:password])
-      log_in @user
-      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
-      redirect_back_or @user
+      if @user.activated?
+        log_in @user
+        params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+        redirect_back_or @user
+      else
+        flash[:warning] = "Activate your account (check your email)"
+        redirect_to root_url
+      end
     else
       flash.now[:danger] = "Invalid email/password"
       render 'new'
@@ -18,4 +24,5 @@ class SessionsController < ApplicationController
     log_out if logged_in?
     redirect_to root_url
   end
+
 end
--- rails/rails_app/app/models/user.rb
+++ rails/rails_app/app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
 
   before_save { email.downcase! }
+  before_create :create_activation_digest
 
   validates :name,
     presence: true,
@@ -37,12 +38,28 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, User.digest(remember_token))
   end
 
-  def authenticated?(remember_token)
-    return false if remember_digest.nil?
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
   end
 
   def forget
     update_attribute(:remember_digest, nil)
   end
+
+  def activate
+    update_columns(activated: true, activated_at: Time.zone.now)
+  end
+
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
+  private
+
+    def create_activation_digest
+      self.activation_token = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
 end
