1af2697be6ce0dbcb2a0d9d2272f07e0a4f60be8
Alexander Luna <alexanderluna1891@gmail.com>
2020-11-06 00:44:36 +0100

    Add: Ajax to the cart and action cable to update the display page

--- rails/depot/app/controllers/store_controller.rb
+++ rails/depot/app/controllers/store_controller.rb
@@ -1,4 +1,7 @@
 class StoreController < ApplicationController
+  include CurrentCart
+  before_action :set_cart
+
   def index
     @page_title = "Moon's Store"
     @products = Product.order(:title)
