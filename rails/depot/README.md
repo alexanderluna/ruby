b5f4cf6a477db3b2723a6748d0ffd7d5bac71b12
Alexander Luna <alexanderluna1891@gmail.com>
2020-11-16 02:24:04 +0100

    Update: gems, seed and setup instructions

--- rails/depot/README.md
+++ rails/depot/README.md
@@ -1,3 +1,13 @@
 # Depository Application
 
 An online store with a depository system.
+
+## Installation and Setup
+
+```bash
+git clone project.git
+cd project
+
+bundle install
+yarn install
+```
--- rails/depot/db/seeds.rb
+++ rails/depot/db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |product|
+    Product.create!(
+        title: "Moon Beer #{product}",
+        description: "Moon Beer #{product} is the best moon beer you can get",
+        price: rand(0..10.0).round(2),
+        image_url: "beer.jpg"
+    )
+end
