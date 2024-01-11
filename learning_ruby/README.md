b23af9c62e0521bec1c543ecac77f58bfe62123b
Alexander Luna <alexanderluna1891@gmail.com>
2024-01-11 17:05:30 +0100

    Update: methods

--- learning_ruby/README.md
+++ learning_ruby/README.md
@@ -1,50 +1,50 @@
 # Learning Ruby
 
+## Overview
+
+1. [Installing Ruby](#installing-ruby)
+2. [Introduction](./introduction.rb)
+3. [Classes and Objects](./classes_objects.rb)
+4. [Methods](./methods.rb)
+
 ## Installing Ruby
 
 1. In order to install Ruby, preferably use a ruby version manager like `rvm` or
 `rbenv`.
 
-```sh
-brew install rbenv ruby-build
-sudo apt install rbenv
-```
+    ```sh
+    brew install rbenv ruby-build
+    sudo apt install rbenv
+    ```
 
 2. Then run this command and follow the instructions.
 
-```sh
-rbenv init
-```
+    ```sh
+    rbenv init
+    ```
 
 3. Now you can install a version. Preferably the latest stable version.
 
-```sh
-# list stable ruby versions
-rbenv install -l
+    ```sh
+    # list stable ruby versions
+    rbenv install -l
 
-rbenv install 3.3.0
-```
+    rbenv install 3.3.0
+    ```
 
 4. If you can run these commands successfully you are all set.
 
-```sh
-(master*) ruby                                                           
-puts "hello"
-hello
-```
+    ```sh
+    (master*) ruby                                                           
+    puts "hello"
+    hello
+    ```
 
-```sh
-(master*) irb
-irb(main):001> 
-```
-
-## Overview
-
-  1. [Introduction](./introduction.rb)
-  2. [Classes and Objects](./classes_objects.rb)
-  3. [Collections and Blocks](./collections_blocks_iterators.rb)
+    ```sh
+    (master*) irb
+    irb(main):001> 
+    ```
 
 ## Resources
 
 - [Programming Ruby 3.2](https://pragprog.com/titles/ruby5/programming-ruby-3-3-5th-edition/)
-
--- learning_ruby/methods.rb
+++ learning_ruby/methods.rb
@@ -1,36 +1,145 @@
+# frozen_string_literal: true
+
+################################################################################
+# In Ruby a methods contain normal ruby expressions. Indentation is not
+# mandatory but considered good style. Definining the same method twice will
+# not raise an error but overwrite it. The last expression is returned by
+# default.
+################################################################################
+
 def welcome
-	puts "Hello World!"
+  puts 'Hello World!'
 end
 
-def add
-	puts 1 + 1
+def hello = puts 'こんにちは世界史'
+
+################################################################################
+# Use snake case for symbols, methods and variables. Methods may end in a ?/!/=
+# - ?: the method returns a boolean
+# - !: dangerous methods which modify the receiver
+# - =: methods that can appear on the left side of the assignment
+################################################################################
+
+1.instance_of?(Integer)
+
+greeting = 'hello world'
+greeting.chop!	# permanently changed to 'hello worl'
+
+def price_in_cents=(cents)
+  @price = cents / 100.0
 end
 
+price_in_cents = 150
 
-#underscore between words like variable names
+################################################################################
+# Ruby allows to define methods for a specific object adding it to the class
+# itself.
+################################################################################
 
-def longes_word
-	words = ["apple", "banana", "pear"]
-
-	longes_word = words.inject do |memo, word|
-		memo.length > word.length ? memo : word
-	end
-	puts longes_word
+class Computer
+  def self.operating_system
+    'Linux'
+  end
 end
 
+mac = Computer.new
+def mac.operating_system = 'macos'
 
-#method names can have question marks in them 
-#useful for tests and booleans
+Computer.operating_system
 
-def over_five?
+################################################################################
+# Methods can have many parameters and you can add default values to them. You
+# can also use a variable length of parameters with an asterisk or use keyword
+# parameters using a hash.
+################################################################################
 
-	value  = 5
+def longest_word
+  words = %w[apple banana pear]
 
-	puts value > 5 ? 'Over 5' : 'Not over 5'
-	
+  words.inject do |memo, word|
+    memo.length > word.length ? memo : word
+  end
 end
 
-welcome
-longes_word
-add
-over_five?
+puts longest_word # banana
+
+def longest_word(first, second, third)
+  [first, second, third].inject do |memo, word|
+    memo.length > word.length ? memo : word
+  end
+end
+
+puts longest_word('apple', 'banana', 'pear') # banana
+
+def longest_word(first = 'apple', second = 'banana', third = 'pear')
+  [first, second, third].inject do |memo, word|
+    memo.length > word.length ? memo : word
+  end
+end
+
+puts longest_word # banana
+
+def longest_word(*words)
+  words.inject do |memo, word|
+    memo.length > word.length ? memo : word
+  end
+end
+
+puts longest_word(%w[apple banana pear]) # banana
+
+def longest_word(words:)
+  words.inject do |memo, word|
+    memo.length > word.length ? memo : word
+  end
+end
+
+puts longest_word(words: %w[apple banana pear]) # banana
+
+################################################################################
+# Methods can receive blocks as parameters and call them using [yield]. When the
+# block parameter is prefixed with an ampersand, Ruby will automatically
+# convert it to a [Proc] so you can call later when you want.
+################################################################################
+
+class Calculator
+  def initialize(value, &block)
+    @value = value
+    @block = block
+  end
+
+  def get_value(amount)
+    @block.call(amount, @value)
+  end
+end
+
+AddCalculator = Calculator.new(5) { |amount, value| amount + value }
+AddCalculator.get_value(10) # 15
+
+################################################################################
+# [Private] methods allow you to call methods only with the current object
+################################################################################
+
+class Invoice
+  def initialize(order)
+    @order = order
+  end
+
+  def print
+    "The total is: #{@oder.value + calculate_tax} (VAT included)"
+  end
+
+  private
+
+  def calculate_tax
+    18.4 + @oder.tax
+  end
+end
+
+################################################################################
+# The ampersand can be used to pass a block as a parameter and convert it to
+# Proc. The reverse is also true. A Proc can be passed to a method and
+# converted to a block.
+################################################################################
+
+%w[hello world].map { |word| word.upcase } # passing a block as argument
+%w[hello world].map(&:upcase) #  passing a proc as argument
